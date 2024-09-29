e291a67c3989225fe4e73657d3a624d43341c252
farrukh2235 <farrukhalam90@gmail.com>
2024-09-29 20:41:48 +0200

    updating the file

--- Anton_agent/models.py
+++ Anton_agent/models.py
@@ -55,45 +55,6 @@ class PrioritizedReplayBuffer:
             self.priorities[idx] = abs(error) + 1e-5  # Avoid zero priority
 
 # Deep Q Network with Batchnormalization and dropout
-class DQNetwork(nn.Module):
-    def __init__(self, state_size, action_size, num_frames = 3):
-        super(DQNetwork, self).__init__()
-        self.conv1 = nn.Conv2d(in_channels=num_frames, out_channels=32, kernel_size=3, stride=1)
-        self.bn1 = nn.BatchNorm2d(32)
-        self.conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, stride=1)
-        self.bn2 = nn.BatchNorm2d(64)
-        # self.conv3 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, stride=1)
-        # self.bn3 = nn.BatchNorm2d(64)
-        
-        conv_output_size = self._get_conv_output(state_size)
-        self.fc1 = nn.Linear(conv_output_size, 256)
-        self.dropout = nn.Dropout(p=0.5)  # Dropout layer after first fully connected layer
-        self.fc2 = nn.Linear(256, 128)
-        self.fc3 = nn.Linear(128, action_size)
-    
-    def _get_conv_output(self, shape):
-        with torch.no_grad():
-            x = torch.zeros(1, 3, shape, shape)
-            x = self.conv1(x)
-            x = self.conv2(x)
-            # x = self.conv3(x)
-            return int(np.prod(x.size()))
-
-    def forward(self, x):
-        x = torch.relu(self.bn1(self.conv1(x)))
-        x = torch.relu(self.bn2(self.conv2(x)))
-        # x = torch.relu(self.bn3(self.conv3(x)))
-        x = x.view(x.size(0), -1)  # Flatten the output from the conv layers
-        x = torch.relu(self.fc1(x))
-        x = self.dropout(x)  # Apply dropout after the first fully connected layer
-        x = torch.relu(self.fc2(x))
-        x = self.fc3(x)  # Final output layer
-        return x
-    
-
-
-
-# Deep Q Network with Batchnormalization and dropout
 class ResidualBlock(nn.Module):
     def __init__(self, in_channels, out_channels, stride=1):
         super(ResidualBlock, self).__init__()
